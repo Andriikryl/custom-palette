c91bceab16f130837d050e9f5f8f4cf6b826fdb4
Andriikryl <andreyk557@gmail.com>
2024-06-02 17:25:59 +0300

    add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import WrapperStore from "@/store/WrapperStore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="wrapper">{children}</main>
+        <WrapperStore>
+          <main>{children}</main>
+        </WrapperStore>
       </body>
     </html>
   );
