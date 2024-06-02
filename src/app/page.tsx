efbc5daa0837e27a3af594e7cdf00612deb9c2b8
Andriikryl <andreyk557@gmail.com>
2024-06-02 17:25:50 +0300

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Controls from "@/components/controls/Controls";
+import Wrapper from "@/components/wrapper/Wrapper";
+import Title from "@/components/title/Title";
+import Palette from "@/components/palette/Palette";
 
 export default function Home() {
   return (
-    <div>
-      <h1>sad</h1>
-    </div>
+    <Wrapper>
+      <>
+        <Title />
+        <Controls />
+        <Palette />
+      </>
+    </Wrapper>
   );
 }
