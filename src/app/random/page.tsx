9d015808fd1597e8846b9527119677aa1419d248
Andriikryl <andreyk557@gmail.com>
2024-06-08 17:04:25 +0300

    add Random page

---
+++ src/app/random/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { range } from "@/utils/range";
+import React, { useEffect, useState } from "react";
+import styles from "./style.module.css";
+import { getRandomHex } from "@/utils/randomHex";
+import { colord } from "colord";
+
+export default function Random() {
+  const [amount, setAmount] = useState(16);
+  const [colors, setColors] = useState<string[]>([]);
+  useEffect(() => {
+    const newColors = range(0, amount).map(() => getRandomHex());
+    setColors(newColors);
+  }, [amount]);
+  function Increment() {
+    setAmount(amount + 1);
+  }
+  function Dicrement() {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    }
+  }
+  return (
+    <div className={styles.random__wrapper}>
+      <h1 className={styles.random__title}>Random Color Generator</h1>
+      <div className={styles.controls}>
+        <button onClick={Dicrement}>-</button>
+        <p>{amount}</p>
+        <button onClick={Increment}>+</button>
+      </div>
+      <div className={styles.palette__wrapper}>
+        {colors.map((color, index) => (
+          <div className={styles.wrapper__box} key={index}>
+            <div
+              className={styles.palette__box}
+              style={{ backgroundColor: color }}
+            ></div>
+            <div className={styles.palette__values}>
+              <p className={styles.hex__color}>hex:{color}</p>
+              <p className={styles.hex__color}>{colord(color).toRgbString()}</p>
+              <p className={styles.hex__color}>{colord(color).toHslString()}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
