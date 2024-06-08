614daeb13a461516e665e8bbe6d9d4574101a557
Andriikryl <andreyk557@gmail.com>
2024-06-08 17:04:15 +0300

    add new Nav links

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,23 +2,53 @@ import React from "react";
 import styles from "./style.module.css";
 import Eye from "@/icons/Eye";
 import Link from "next/link";
+import { range } from "@/utils/range";
 
 export default function Nav() {
+  const generateRandomColor = (): string =>
+    `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")}`;
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.icon__box}>
-        <Eye size={50} />
+    <div className={styles.nav__wrapper}>
+      <div className={styles.palete__box}>
+        {range(1, 20).map((item, index) => {
+          return (
+            <div
+              style={{ backgroundColor: generateRandomColor() }}
+              className={styles.inner__box}
+              key={index}
+            ></div>
+          );
+        })}
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/scheme"}>Scheme</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.flow}>
+        <nav>
+          <ul role="list">
+            <li>
+              <Link className={styles.link} href={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} href={"/scheme"}>
+                Color Scheme Builder
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} href={"/random"}>
+                Random Color Generator
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} href={"/mixer"}>
+                Color Mixer
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
